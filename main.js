600a901bdac45bea873ccfe8bc873384f397a4d1
gksdmswn0315 <haneunju020315@gmail.com>
2021-11-24 19:02:13 +0900

    finally

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //넷바 색상 변경
 // let :변하는 상수 | const :변하지 않는 상수
-const navbar =document.querySelector('#navbar')
+const navbar = document.querySelector('#navbar')
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
     if(window. scrollY > navbarHeight){
@@ -31,27 +31,11 @@ contactMeBtn.addEventListener("click", ()=> {
     scrollIntoView('#contact');
 });
 
-//arrow-up 버튼 이동하기
-const arrowUp = document.querySelector(".arrow-up");
-arrowUp.addEventListener("click", ()=> {
-    document.querySelector("#home").scrollIntoView({behavior : 'smooth'});
-});
-
 //home 투명도 조절
 const home = document.querySelector(".home__container")
-const homeHeight = home.getBoundingClientRect().height; // 홈 세로 길이 구하는 명령어
+const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", ()=>{
     home.style.opacity = 1 - window.scrollY/homeHeight;
-
-    //home의 1/2가 스크롤 되었을 때 arrow-up 버튼 생성
-    if (window.scrollY > homeHeight / 2) { 
-        arrowUp.style.opacity = 1;
-    }
-
-    //home이 모두 보일 경우 arrow-up 버튼 소멸
-    if (window.scrollY < 1) {
-        arrowUp.style.opacity = 0;
-    }
 });
 
 //햄버거 메뉴 클릭할 때
@@ -60,6 +44,63 @@ navbarToggleBtn.addEventListener("click", ()=>{
     navbarMenu.classList.toggle("open");
 });
 
+//??
+document.addEventListener('scroll', function() {
+    var currentScrollValue = document.documentElement.scrollTop;
+    console.log('currentScrollValue is ' + currentScrollValue);
+});
+
+// arrow up 버튼 스크롤 될 때 생성
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+    if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+    } else {
+    arrowUp.classList.remove("visible");
+    }
+});
+
+// arrow up 버튼 클릭했을 때 home으로 올라가기
+arrowUp.addEventListener("click", () => {
+    scrollIntoView("#home");
+});
+
+// 프로젝트 js 부분
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projects.forEach((project) => {
+        if (filter === "*" || filter === project.dataset.type) {
+            project.classList.remove("invisible");
+        } else {
+            project.classList.add("invisible");
+        }
+    });
+    const active = document.querySelector(".category__btn.selected");
+    if (active != null) {
+        active.classList.remove("selected");
+    }
+    e.target.classList.add("selected");
+    projectContainer.classList.add("anim-out");
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if (filter === "*" || filter === project.dataset.type) {
+                project.classList.remove("invisible");
+            } else {
+                project.classList.add("invisible");
+            }
+        });
+        projectContainer.classList.remove("anim-out");
+    }, 300);
+});
+
+//??
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior : 'smooth'});
